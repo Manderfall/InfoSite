4195223058a74e270549eb07caf2f4ca2381d4b4
Manderfall <Manderfall@gmail.com>
2020-09-12 17:31:00 +0000

    Added Navbar

---
+++ app.js
@@ -0,0 +1,49 @@
+// General Imports and setup //
+const express = require ("express");
+const bodyParser = require("body-parser");
+const mongoose = require ("mongoose");
+const methodOverride = require ("method-override");
+
+
+// Express and Modules ------------------------------------------
+const app = express();
+app.use(express.static("public")); // js, css, etc.
+app.set("view engine", "ejs"); // EJS is a dependency
+
+
+// Express
+app.use(require("express-session")({
+    secret: "Tiny Dancer",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method")); // For PUT requests
+
+
+// Mongoose ----------------------------------------------------
+
+mongoose.connect('mongodb://localhost:27017/infosite', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
+
+
+// Routes ------------------------------------------
+const indexRoutes = require("./routes/index");
+
+
+
+app.use(indexRoutes);
+
+// Globals ------------------------------------------------------
+const port = 3000;
+
+
+
+
+// START/LISTEN//		
+app.listen(port, () => { console.log(`Listening on port ${port}`); });
+	
