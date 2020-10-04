237ff954c9155a504392e551e15f091c98bc31c4
Manderfall <Manderfall@gmail.com>
2020-10-04 15:25:26 +0000

    made navbar responsive

---
+++ public/js/navbar.js
@@ -0,0 +1,10 @@
+var myNav = document.getElementById("nav");
+
+window.onscroll = function() {
+  "use strict";
+  if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+    myNav.classList.add("scroll");
+  } else {
+    myNav.classList.remove("scroll");
+  }
+};
